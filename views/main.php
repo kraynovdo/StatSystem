743d9d6fb7baacf98548a864f4f7cb5578408242
kraynovdo <kraynovdo@gmail.com>
2017-04-11 09:30:07 +0300

    правки заявки в мобильной версии

--- views/main.php
+++ views/main.php
@@ -16,7 +16,7 @@
         <link rel="stylesheet" type="text/css" href="//<?=$HOST?>/themes/<?=$theme?>/<?=$theme?>.css?9"/>
     <?}?>
     <?if ($IS_MOBILE) {?>
-        <link rel="stylesheet" type="text/css" href="//<?=$HOST?>/themes/main_mobile_s.css?37"/>
+        <link rel="stylesheet" type="text/css" href="//<?=$HOST?>/themes/main_mobile_s.css?38"/>
     <?}?>
     <link rel="shortcut icon" href="//<?=$HOST?>/themes/img/fafr_logo.png" type="image/x-icon" />
     <link rel="stylesheet" href="//<?=$HOST?>/jquery/jquery-ui.css">
@@ -53,7 +53,7 @@ else {
 
 <script type="text/javascript" src="//<?=$HOST?>/themes/components.js?31"></script>
 <?if (file_exists($_SERVER['DOCUMENT_ROOT'] . $CONSTPath  . '/views/' . $controller . '/' . $controller . '.js')) {?>
-    <script type="text/javascript" src="//<?=$HOST?>/views/<?=$controller?>/<?=$controller?>.js?34"></script>
+    <script type="text/javascript" src="//<?=$HOST?>/views/<?=$controller?>/<?=$controller?>.js?35"></script>
 <?}?>
 
 </body>
