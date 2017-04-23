2925f513ef03b4612becafbfc0d153d12b6d374a
kraynovdo <kraynovdo@gmail.com>
2017-04-23 14:27:40 +0300

    понятный список команд для прав

--- views/userteam/add.php
+++ views/userteam/add.php
@@ -4,9 +4,16 @@
     <div>
         <select name="team" data-validate="req" class="userteam_team">
             <option value="">Выберите команду</option>
-            <?; for($i = 0; $i < count($team); $i++) {?>
-                <option value="<?=$team[$i]['id']?>"><?=$team[$i]['rus_name']?></option>
-            <?}?>
+            <?
+
+            for ($i = 0; $i < count($team); $i++) {
+                $age = '';
+                if ($team[$i]['age'] != 'Взрослые') {
+                    $age = ', ' . $team[$i]['age'] . '';
+                }
+                ?>
+                <option value="<?=$team[$i]['id']?>"><?=$team[$i]['rus_name']?> (<?=$team[$i]['city']?><?=$age?>)</option>
+            <?   }?>
         </select>
     </div>
     <input type="button" class="main-btn main-submit" value="Добавить"/>
