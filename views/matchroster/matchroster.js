c7b1e723197337a8fc2c7231ec6aa1ddb422dc05
kraynovdo <kraynovdo@gmail.com>
2017-05-01 22:14:50 +0300

    правки по проверке состава

--- views/matchroster/matchroster.js
+++ views/matchroster/matchroster.js
@@ -28,18 +28,17 @@ $(function () {
 
     $('.refcheck_confirm').click(function(){
         var id = $(this).attr('data-id');
-        var self = this;
         $.post('/?r=match/confirm', {match : id}, function(res){
             var code = parseInt(res, 10);
             if (code == 1) {
-                $(self).text('Открыть на изменение');
+                $('.refcheck_confirm').text('Открыть на изменение');
                 $('.refcheck-change').addClass('main-hidden');
                 $('.refcheck-number').removeClass('main-hidden');
                 $('.refcheck-inputCont').addClass('main-hidden');
                 $('.refcheck-ok').addClass('main-hidden');
             }
             else if (code == 0) {
-                $(self).text('Закрыть от изменений');
+                $('.refcheck_confirm').text('Закрыть от изменений');
                 $('.refcheck-change').removeClass('main-hidden');
             }
         })
