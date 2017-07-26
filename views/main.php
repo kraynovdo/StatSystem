d21884ce3f8e901a2320d51f0b65937bba7a6c9b
kraynovdo <kraynovdo@gmail.com>
2017-07-26 19:25:22 +0300

    выделил ссылки более синим цветом

--- views/main.php
+++ views/main.php
@@ -11,7 +11,7 @@
         <script type="text/javascript">window.mobile = true;</script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <?}?>
-    <link rel="stylesheet" type="text/css" href="//<?=$HOST?>/themes/main.css?136"/>
+    <link rel="stylesheet" type="text/css" href="//<?=$HOST?>/themes/main.css?137"/>
     <?if ($theme) {?>
         <link rel="stylesheet" type="text/css" href="//<?=$HOST?>/themes/<?=$theme?>/<?=$theme?>.css?9"/>
     <?}?>
