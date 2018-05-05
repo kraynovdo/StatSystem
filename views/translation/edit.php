3718b1270581f56d78a5f5415beae333bb1f7674
kraynovdo <kraynovdo@gmail.com>
2018-05-05 15:12:59 +0300

    избавился от сущности трансляции

--- views/translation/edit.php
+++
@@ -1,9 +0,0 @@
-<form method="POST" action="/?r=translation/update">
-    <input type="hidden" name="trans" value="<?=$_GET['trans']?>"/>
-    <input type="hidden" name="comp" value="<?=$_GET['comp']?>"/>
-    <div>Заголовок</div>
-    <input class="trans_input" type="text" name="title" value="<?=$answer['title']?>"/>
-    <div>Код плеера</div>
-    <input class="trans_input" type="text" name="link" value='<?=$answer['link']?>'/>
-    <input type="button" class="main-btn main-submit" value="OK"/>
-</form>
