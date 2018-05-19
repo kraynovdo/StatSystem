c0fa152df8474a53a40d07aaa559ad809058b0de
kraynovdo <kraynovdo@gmail.com>
2018-05-19 13:44:02 +0300

    поправил форму логина

--- views/user/login.php
+++ views/user/login.php
@@ -1,11 +1,19 @@
 <?if (!$IS_MOBILE) {?>
     <?if (!$_SESSION['userID']) {?>
     <form action="/?r=user/auth" method="POST">
-        <a href="/?r=registration" class="login-regLink">Регистрация</a>
-        <input class='main-auth_login' type="text" name="username" autocomplete="false" placeholder="E-mail"/>
-        <input  class='main-auth_pass' type="password" name="password" autocomplete="false" placeholder="Пароль"/>
-        <input class="main-btn main-auth_btn" type="submit" value="Вход"/>
-        <a href="/?r=user/forget">Забыли пароль?</a>
+        <div>
+            <a href="/?r=registration" class="login-regLink">Регистрация</a>
+        </div>
+        <div>
+            <input class='main-auth_login' type="text" name="username" autocomplete="false" placeholder="E-mail"/>
+        </div>
+        <div>
+            <input  class='main-auth_pass' type="password" name="password" autocomplete="false" placeholder="Пароль"/>
+            <input class="main-btn main-auth_btn" type="submit" value="Вход"/>
+        </div>
+        <div>
+            <a href="/?r=user/forget">Забыли пароль?</a>
+        </div>
     </form>
     <?} else {?>
         <a href="/?r=registration" class="login-regLink">Регистрация</a>
