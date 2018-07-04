1956febdfbe6551459d7124e1c0d3e83666e6dbe
kraynovdo <kraynovdo@gmail.com>
2018-07-04 20:36:40 +0300

    метрика на новом аккаунте

--- views/YaMetrics.php
+++ views/YaMetrics.php
@@ -4,8 +4,8 @@
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
             try {
-                w.yaCounter28709096 = new Ya.Metrika({
-                    id:28709096,
+                w.yaCounter49474156 = new Ya.Metrika2({
+                    id:49474156,
                     clickmap:true,
                     trackLinks:true,
                     accurateTrackBounce:true,
@@ -19,13 +19,13 @@
             f = function () { n.parentNode.insertBefore(s, n); };
         s.type = "text/javascript";
         s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
 
         if (w.opera == "[object Opera]") {
             d.addEventListener("DOMContentLoaded", f, false);
         } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
+    })(document, window, "yandex_metrika_callbacks2");
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/28709096" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<noscript><div><img src="https://mc.yandex.ru/watch/49474156" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 <?}?>
