b8d4b172923523d7901dcb932b037e25276f8a1b
kraynovdo <kraynovdo@gmail.com>
2018-07-06 21:11:43 +0300

    все сюда кликают

--- views/competition/start.php
+++ views/competition/start.php
@@ -1,7 +1,7 @@
 <div class="fafr-minWidth fafr-maxWidth">
     <div class="comp-teamList">
         <?$filter = $_GET['comp'] ? '&comp='.$_GET['comp'] : ''; $teamlist = $answer['teamlist']; for ($i = 0; $i < count($teamlist); $i++) {?>
-            <a class="comp-teamList_item" target="_blank" href1="/?r=team/view&team=<?=$teamlist[$i]['id']?><?=$filter?>">
+            <a class="comp-teamList_item" target="_blank" href="/?r=team/view&team=<?=$teamlist[$i]['id']?><?=$filter?>">
                 <img alt="<?=$teamlist[$i]['rus_name']?>" title="<?=$teamlist[$i]['rus_name']?>" class="comp-teamList_img" src="//<?=$HOST?>/upload/<?=$teamlist[$i]['logo']?>"/>
             </a>
         <?}?>
